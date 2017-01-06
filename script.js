7b8526a9835336d918b093a5b2aa5374bfd1d58a
Michael Lefkowitz <lefkowitz.michael@gmail.com>
2017-01-05 19:25:52 -0600

    Script runs and pulls cart OL

---
+++ script.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Great Courses OOS
+// @namespace    http://lefkowitz.me/
+// @version      0.1
+// @description  Pulls first item in shopping cart, displays that item as OOS alongside addtional information
+// @author       Michael Lefkowitz
+// @match        http://www.thegreatcourses.com/category/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log('Script running');
+    console.log(document.getElementById("cart-sidebar"));
+})();
