1437d34f7f955cbfcefc5beb410b898ed713b52f
Michael Lefkowitz <lefkowitz.michael@gmail.com>
2017-01-08 20:06:09 -0600

    Prepends simple message to top

--- script.js
+++ script.js
@@ -6,12 +6,11 @@
 // @author       Michael Lefkowitz
 // @match        http://www.thegreatcourses.com/category/*
 // @grant        none
+// @require      https://code.jquery.com/jquery-1.12.4.js
 // ==/UserScript==
 
 (function() {
     'use strict';
-
-    console.log('Script running');
 
     // gets first "li" in the cart-sidebar "ol"
     const firstItem = document.getElementById("cart-sidebar").firstElementChild;
@@ -19,7 +18,6 @@
 
     // finds the "a" tag in first "li", then get its "title" attribute
     const firstItemTitle = firstItem.getElementsByTagName("a")[0].getAttribute("title");
-    console.log(`${firstItemTitle} is almost out of stock!`);
 
     const request = new XMLHttpRequest();
     request.open('GET', `http://localhost:3000/search/${firstItemTitle}`, true);
@@ -27,15 +25,18 @@
     request.onload = () => {
         if (request.status >= 200 && request.status < 400) {
             const data = JSON.parse(request.responseText);
+            let message = `${firstItemTitle} is almost out of stock!`;
+            console.log(message);
             if (data.price) {
-              // if item is on sale, show full price and sale price
               console.log(`Get it now while it's on sale for $${data.price}!`);
               console.log(`before it goes back to the orignal price of $${data.top}`);
+              $('body').append('<div id="dialog34" title="Basic dialog"><p>This is the default dialog which is useful for displaying information. The dialog window can be moved, resized and closed with the icon.</p></div>');
             } else {
-              // if item isn't on sale, show full price and fake coupon
               console.log(`Grab it now with a 20% off coupon code - SAVENOW20`);
               const currentPrice = firstItem.querySelector(".price").innerHTML.slice(1);
               console.log(`bringing your price down to $${+currentPrice * 0.8} from the original price of $${currentPrice}`);
+              $('body').prepend(`<div id="coupon-dialog" style="position:fixed;background:url(http://www.thegreatcourses.com/skin/frontend/enterprise/tgc/images/tgc/main-bg.jpg);z-index:100;width:100%;text-align:center;">${message}</div>`);
+              $('.coupon-message').html('&nbsp;');
             }
         } else {
             console.log('Error!');
